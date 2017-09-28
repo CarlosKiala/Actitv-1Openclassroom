7c7604d8c4b2789d83f32c8e3ef17e97c8f5011d
casrlos <castelo.k@hotmail.be>
2017-09-28 15:34:40 +0200

    Création des deux liste quotes et characters du tuto sur python

---
+++ san_antonio.py
@@ -0,0 +1,14 @@
+
+
+quotes = [ 
+        "Ecoutez-moi, Monsieur Shakespeare, nous avons beau etre ou ne pas etre, nous sommes !",
+        "On doit pouvoir chosiir entre s'écouter parler et se faire entendre." ]
+
+characters = [ 
+        "alvin et les Chipmunks",
+        "Babar",
+        "betty boop",
+        "calimero",
+        "casper",
+        "le chat potté"
+        "Kirikou"
